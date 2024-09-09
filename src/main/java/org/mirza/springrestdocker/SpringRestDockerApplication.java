e56777a4ecff0a652ea34b63e45b40958327b320
mirz <mirza.romi@doku.com>
2024-09-09 08:15:52 +0700

    first commit

---
+++ src/main/java/org/mirza/springrestdocker/SpringRestDockerApplication.java
@@ -0,0 +1,13 @@
+package org.mirza.springrestdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mirza/springrestdocker/SpringRestDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mirza.springrestdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
